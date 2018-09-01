0f839948b008436289cabb75f95b7cb2569f683b
HL <>
2018-08-31 21:45:07 -0400

    Add inputs for start and end

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import * as Utils from './utils';
 import * as Status from './status';
 class App extends Component {
   state = {
-    status: Status.READY
+    status: Status.READY,
+    start: null,
+    end: null
   };
+
+  handleChangeInputStart = event =>
+    this.setState({ start: event.target.value });
+  handleChangeInputEnd = event => this.setState({ end: event.target.value });
 
   getAnalytics = async () => {
     try {
+      const { start, end } = this.state;
       this.setState({ status: Status.REQUESTED });
-      const res = await Utils.getAnalytics();
+      const radix = 10;
+      const res = await Utils.getAnalytics(
+        parseInt(start, radix),
+        parseInt(end, radix)
+      );
       this.setState({ ...res, status: Status.SUCCEEDED });
     } catch (e) {
       console.log(e);
@@ -19,7 +30,7 @@ class App extends Component {
   };
 
   render() {
-    const { status } = this.state;
+    const { status, start, end } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -29,8 +40,18 @@ class App extends Component {
         <p>Status: {status}</p>
 
         <h2>Start and End Range of block numbers (inclusive)</h2>
-        <input type="number" placeholder="Start" />
-        <input type="number" placeholder="End" />
+        <input
+          type="number"
+          placeholder="Start"
+          value={start}
+          onChange={this.handleChangeInputStart}
+        />
+        <input
+          type="number"
+          placeholder="End"
+          value={end}
+          onChange={this.handleChangeInputEnd}
+        />
         {status !== Status.REQUESTED && (
           <button onClick={this.getAnalytics}>Analyze the blockchain!</button>
         )}
