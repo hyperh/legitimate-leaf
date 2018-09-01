adae8e4b51a030e1c4ce50e1689cfd728ce15ce2
HL <>
2018-09-01 00:21:05 -0400

    Fix react key warning

--- src/Results.js
+++ src/Results.js
@@ -15,7 +15,7 @@ const Results = ({
     <div>
       <h3>Amount ETH received by address:</h3>
       {Object.keys(receiverTotals).map(address => (
-        <div>
+        <div key={address}>
           {address}: {web3.utils.fromWei(receiverTotals[address])}
         </div>
       ))}
@@ -23,7 +23,7 @@ const Results = ({
     <div>
       <h3>Amount ETH sent by address:</h3>
       {Object.keys(senderTotals).map(address => (
-        <div>
+        <div key={address}>
           {address}: {web3.utils.fromWei(senderTotals[address])}
         </div>
       ))}
