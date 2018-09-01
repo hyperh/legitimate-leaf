5ffd4ad39f15aa5fd74e8fbee11d7f8723796a48
HL <>
2018-08-31 23:02:23 -0400

    Add separate fn to get prev blocks analytics

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import * as Utils from './utils';
 import * as Status from './status';
+
 class App extends Component {
   state = {
     status: Status.READY,
     start: null,
-    end: null
+    end: null,
+    diff: null
   };
 
   handleChangeInputStart = event =>
     this.setState({ start: event.target.value });
   handleChangeInputEnd = event => this.setState({ end: event.target.value });
 
-  getAnalytics = async () => {
+  getAnalyticsWithStartEnd = async () => {
     try {
       const { start, end } = this.state;
       this.setState({ status: Status.REQUESTED });
@@ -29,8 +31,28 @@ class App extends Component {
     }
   };
 
+  getAnalyticsWithDiff = async () => {
+    try {
+      const { diff } = this.state;
+      const radix = 10;
+
+      this.setState({ status: Status.REQUESTED });
+
+      const currentBlockNum = await Utils.getBlockNumber();
+
+      const res = await Utils.getAnalytics(
+        parseInt(currentBlockNum - diff, radix),
+        parseInt(currentBlockNum, radix)
+      );
+      this.setState({ ...res, status: Status.SUCCEEDED });
+    } catch (e) {
+      console.log(e);
+      this.setState({ status: Status.FAILED });
+    }
+  };
+
   render() {
-    const { status, start, end } = this.state;
+    const { status, start, end, diff } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -53,16 +75,22 @@ class App extends Component {
           onChange={this.handleChangeInputEnd}
         />
         {status !== Status.REQUESTED && (
-          <button onClick={this.getAnalytics}>Analyze the blockchain!</button>
+          <button onClick={this.getAnalyticsWithStartEnd}>
+            Analyze the blockchain!
+          </button>
         )}
 
-        <h2>X previous blocks</h2>
+        <h2>Number of blocks before present block</h2>
         <input
           type="number"
           placeholder="Number of blocks before present block"
+          value={diff}
+          onChange={this.handleChangeInputDiff}
         />
         {status !== Status.REQUESTED && (
-          <button onClick={this.getAnalytics}>Analyze the blockchain!</button>
+          <button onClick={this.getAnalyticsWithDiff}>
+            Analyze the blockchain!
+          </button>
         )}
 
         <p>{JSON.stringify(this.state.res, null, 2)}</p>
