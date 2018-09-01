bac2d93a6cfb379b3b88cdf672bdb42a57f66f27
HL <>
2018-09-01 00:11:50 -0400

    Format results better for receivers and senders

--- src/Results.js
+++ src/Results.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import web3 from './web3';
 
-const Results = ({ totalWeiTransferred }) => (
+const Results = ({
+  totalWeiTransferred,
+  receiverTotals,
+  senderTotals,
+  uniqueAddressesIsContract
+}) => (
   <div>
     <h2>Results</h2>
     <div>
       Total ether transferred: {web3.utils.fromWei(totalWeiTransferred)}
     </div>
     <div>
-      <h3>Receivers</h3>
+      <h3>Amount ETH received by address:</h3>
+      {Object.keys(receiverTotals).map(address => (
+        <div>
+          {address}: {web3.utils.fromWei(receiverTotals[address])}
+        </div>
+      ))}
+    </div>
+    <div>
+      <h3>Amount ETH sent by address:</h3>
+      {Object.keys(senderTotals).map(address => (
+        <div>
+          {address}: {web3.utils.fromWei(senderTotals[address])}
+        </div>
+      ))}
     </div>
     <div />
   </div>
